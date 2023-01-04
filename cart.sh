233d3bd185ea0f9558405d7414a63da4b1bcfdcb
manudevops90 <manu.devops@gmail.com>
2023-01-04 20:56:58 +0530

    roboshob full file first time

--- cart.sh
+++ cart.sh
@@ -1,11 +1,11 @@
  set -e
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash >/tmp/cart.log
 
- yum install nodejs -y >/tmp/cart.log
+ yum install nodejs -y  >/tmp/cart.log
  useradd roboshop  >/tmp/cart.log
 
- curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" >/tmp/cart.log
- cd /home/roboshop >/tmp/cart.log
+ curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  >/tmp/cart.log
+ cd /home/roboshop  >/tmp/cart.log
  rm -rf cart >/tmp/cart.log
  unzip -o /tmp/cart.zip  >/tmp/cart.log
  mv cart-main cart >/tmp/cart.log
