83ac9b537f1f28203576905070324f6bb22bb01a
manudevops90 <manu.devops@gmail.com>
2023-01-05 06:55:55 +0530

    roboshob full file first time

--- cart.sh
+++ cart.sh
@@ -1,20 +1,32 @@
  set -e
+ echo setting nodejs repos
  curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/cart.log
+echo installing nodejs
  yum install nodejs -y   &>>/tmp/cart.log
-  
+
+  echo adding application user
  useradd roboshop  &>>/tmp/cart.log
 
+echo dowloading content
  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>/tmp/cart.log
- cd /home/roboshop  &>>/tmp/cart.log
+  cd /home/roboshop  &>>/tmp/cart.log
+
+ echo cleaning old application content
  rm -rf cart &>>/tmp/cart.log
+
+ echo extract application archive
  unzip -o /tmp/cart.zip  &>>/tmp/cart.log
  mv cart-main cart &>>/tmp/cart.log
  cd cart &>>/tmp/cart.log
+
+ echo installing nodejs Dependencies
  npm install &>>/tmp/cart.log
 
  #update ips
-
+echo configuring cart systemd services
  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
  systemctl daemon-reload &>>/tmp/cart.log
+
+ echo starting cart services
  systemctl start cart &>>/tmp/cart.log
  systemctl enable cart &>>/tmp/cart.log
