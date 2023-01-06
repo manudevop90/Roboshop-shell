96ceeb60baa73e63531a140b1f8a3f0c2caf16c7
manudevops90 <manu.devops@gmail.com>
2023-01-06 20:22:54 +0530

    roboshob full file first time

--- Mysql.sh
+++ Mysql.sh
@@ -1,8 +1,6 @@
 set -e
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
 
-dnf module disable mysql
-
 yum install mysql-community-server -y
 
 systemctl enable mysqld
@@ -11,9 +9,6 @@ systemctl start mysqld
 DEFAILT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
 echo alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1'; | mysql -uroot -p${DEFAILT_PASSWORD}
-
-
-
 
 > uninstall plugin validate_password;
 
