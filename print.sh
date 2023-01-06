0a6c4f2b948a75a45fe4ee21a615106b6261d5e9
manudevops90 <manu.devops@gmail.com>
2023-01-06 17:03:02 +0530

    roboshob full file first time

--- print.sh
+++ print.sh
@@ -18,3 +18,20 @@ echo -e "\e[34mhello\e[0m"
 echo -e "\e[35mhello\e[0m"
 echo -e "\e[36mhello\e[0m"
 
+#syntax:  -e to enable \e
+#         \e[31m, 31 is the color code
+#         \e[0m, 0 is to reset the color
+## note: in shell when we enable any color andd it is our responsibility to disable it.
+
+## colors    #code
+# RED         31
+# GREEN       32
+# YELLOW      33
+# BLUE        34
+# MAGENTA     35
+# CYAN        36
+
+
+# WE PREFER ALWAYS TO GO WITH DOUBLE QUOTES
+
+#VARIABLES
---
+++ vars.sh
@@ -0,0 +1,7 @@
+a=10
+echo value of a = $a
+date=2022-6-1
+echo today date is $date
+date1=$(date +%F)
+echo todaye ate is $date1
+
