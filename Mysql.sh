8d09673fc97e623609da5d3b3837bbbd6a1be238
manudevops90 <manu.devops@gmail.com>
2023-01-06 20:25:45 +0530

    roboshob full file first time

--- Mysql.sh
+++ Mysql.sh
@@ -1,6 +1,6 @@
 set -e
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-
+dnf module disable mysql
 yum install mysql-community-server -y
 
 systemctl enable mysqld
