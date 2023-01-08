7cc178be7849da97d1cd0c835c66a9943185209b
manudevops90 <manu.devops@gmail.com>
2023-01-08 08:47:39 +0530

    push code

--- Mysql.sh
+++ Mysql.sh
@@ -1,5 +1,7 @@
 set -e
+
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+dnf module disable mysql
 
 yum install mysql-community-server -y
 
