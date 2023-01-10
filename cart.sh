7b1cd691a359b9befb8c3ffc882308cf284f8184
manudevops90 <manu.devops@gmail.com>
2023-01-10 07:11:19 +0530

    push code

--- cart.sh
+++ cart.sh
@@ -1,44 +1,77 @@
 
 echo setting nodejs repos
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/cart.log
-echo $?
-if echo $? -eq 0
-then
-  echo \e
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 echo installing nodejs
 yum install nodejs -y   &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo adding application user
 useradd roboshop  &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo dowloading content
 curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"  &>>/tmp/cart.log
 cd /home/roboshop  &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo cleaning old application content
 rm -rf cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo extract application archive
 unzip -o /tmp/cart.zip  &>>/tmp/cart.log
 mv cart-main cart &>>/tmp/cart.log
 cd cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo installing nodejs Dependencies
 npm install &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fiecho $?
 
  #update ips
 echo configuring cart systemd services
 mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service &>>/tmp/cart.log
 systemctl daemon-reload &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
 
 echo starting cart services
 systemctl start cart &>>/tmp/cart.log
 systemctl enable cart &>>/tmp/cart.log
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+fi
