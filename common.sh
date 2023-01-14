6d291bc9bef40df76bde539dc068cee196992fce
manudevops90 <manu.devops@gmail.com>
2023-01-14 20:26:05 +0530

    push code

--- common.sh
+++ common.sh
@@ -35,7 +35,7 @@ statuscheck
 
 echo extract application archive
 unzip -o /tmp/${COMPONENT}.zip &>>/tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>>/tmp/${COMPONENT}.log && cd /home/roboshop/${COMPONENT} &>>/tmp/${COMPONENT}.log
-statuscheck
+echo $?
 
 echo installing nodejs Dependencies
 npm install &>>/tmp/${COMPONENT}.log
