e3997d12697a2273c6d716b77338a6975c132ff1
manudevops90 <manu.devops@gmail.com>
2023-01-14 22:37:33 +0530

    push code

--- common.sh
+++ common.sh
@@ -1,9 +1,9 @@
 # this script is only for dry
 statuscheck() {
 if [ $? -eq 0 ]; then
-  echo -e "\e[32msuccess\e[0m"
+  echo -e "\e[32mSUCCESS\e[0m"
 else
-  echo -e "\e[31mFail\e[0m"
+  echo -e "\e[31mfaile\e[0m"
   exit 1
 fi
 }
@@ -19,26 +19,30 @@ yum install nodejs -y  &>>/tmp/${COMPONENT}.log
 statuscheck
 
 id roboshop &>>/tmp/${COMPONENT}.log
- if [ $? -ne 0 ]; then
+if [ $? -ne 0 ]; then
    echo adding application user
    useradd roboshop  &>>/tmp/${COMPONENT}.log
-statuscheck
+   statuscheck
 fi
 
 echo dowloading content
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip"  &>>/tmp/${COMPONENT}.log
- cd /home/roboshop  &>>/tmp/${COMPONENT}.log
- statuscheck
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip"  &>>/tmp/${COMPONENT}.log && cd /home/roboshop  &>>/tmp/${COMPONENT}.log
+statuscheck
 
 echo cleaning old application content
- rm -rf ${COMPONENT} &>>/tmp/${COMPONENT}.log
- statuscheck
+rm -rf ${COMPONENT} &>>/tmp/${COMPONENT}.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32mSUCCESS\e[0m"
+else
+  echo -e "\e[31mfaile\e[0m"
+  exit 1
+fi
 
 echo extract application archive
-unzip -o /tmp/${COMPONENT}.zip &>>/tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>>/tmp/${COMPONENT}.log && cd /home/roboshop/${COMPONENT} &>>/tmp/${COMPONENT}.log
+unzip -o /tmp/${COMPONENT}.zip &>>/tmp/${COMPONENT}.log && mv ${COMPONENT}-main ${COMPONENT} &>>/tmp/${COMPONENT}.log && cd ${COMPONENT} &>>/tmp/${COMPONENT}.log
 statuscheck
 
 echo installing nodejs Dependencies
 npm install &>>/tmp/${COMPONENT}.log
- echo $?
+statuscheck
  }
