38900358dc61285e2eca47a81efe6460e5894fcb
manudevops90 <manu.devops@gmail.com>
2023-01-14 13:48:53 +0530

    push code

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,17 +1,82 @@
  set -e
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
- yum install nodejs -y
- useradd roboshop
+ echo settings nodejs repo
+ curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+  echo -e "\e[32msuccess\e[0m"
+else
+  echo -e "\e[31mFail\e[0m"
+  exit 1
+fi   
 
- curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
- cd /home/roboshop
- rm -rf catalogue
- unzip -o /tmp/catalogue.zip
- mv catalogue-main catalogue
- cd /home/roboshop/catalogue
- npm install
+echo installing nodejs repo
+yum install nodejs -y &>>/tmp/catalogue.log
+if [ $? -eg 0 ]; then
+  echo -e "\e[32msuccess\e[0m"
+  else
+    echo -e "\e[31mFail\e[0m"
+    exit 1
+ fi   
+id roboshop &>>/tmp/catalogue.log
+if [ $? -ne 0]; then
+echo adding application user
+ useradd roboshop &>>/tmp/catalogue.log
+ if [ $? -eq 0]; then
+   echo -e "\e[32msuccess\e[0m"
+else
+  echo -e "\e[31mFail\e[0m"
+  exit 1
+  fi
 
- mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
- systemctl daemon-reload
- systemctl start catalogue
- systemctl enable catalogue
+echo download content
+ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/catalogue.log
+ cd /home/roboshop &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
+ echo cleaning old application content
+ rm -rf catalogue &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
+ echo extract application archive
+ unzip -o /tmp/catalogue.zip &>>/tmp/catalogue.log
+ mv catalogue-main catalogue &>>/tmp/catalogue.log
+ cd /home/roboshop/catalogue &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
+ echo installing nodejs dependencies
+ npm install &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
+echo configure catalogue systemd services
+ mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>/tmp/catalogue.log
+ systemctl daemon-reload &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
+ echo starting catalogue services
+ systemctl start catalogue &>>/tmp/catalogue.log
+ systemctl enable catalogue &>>/tmp/catalogue.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccuess\e[0m"
+    else
+    echo -e "\e31Fail\e[0m"
+    exit 1
+fi
