7e2a9bff322dd76364fe4aaf2517c326b6050cd1
manudevops90 <manu.devops@gmail.com>
2023-01-19 19:54:55 +0530

    push code

--- user.sh
+++ user.sh
@@ -1,19 +1,5 @@
- set -e
- curl -sL https://rpm.nodesource.com/setup_lts.x | bash
- yum install nodejs -y
- useradd roboshop
+source common.sh
 
- curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
- cd /home/roboshop
-  rm -rf user
- unzip -o /tmp/user.zip
- mv user-main user
- cd /home/roboshop/user
- npm install
+COMPONENT=user
 
- #update ips
-
- mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
- systemctl daemon-reload
- systemctl start user
- systemctl enable user
+NODEJS
