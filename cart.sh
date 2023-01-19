5dda65603a173853da9ace992551e6593440183a
manudevops90 <manu.devops@gmail.com>
2023-01-19 19:44:07 +0530

    push code

--- cart.sh
+++ cart.sh
@@ -5,10 +5,10 @@ COMPONENT=cart
 NODEJS
 
  #update ips
-echo configuring ${COMPONENT} systemd services
-mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>/tmp/${COMPONENT}.log && systemctl daemon-reload &>>/tmp/${COMPONENT}.log
-statuscheck
+#echo configuring ${COMPONENT} systemd services
+#mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>/tmp/${COMPONENT}.log && systemctl daemon-reload &>>/tmp/${COMPONENT}.log
+#statuscheck
 
-echo starting ${COMPONENT} services
-systemctl start ${COMPONENT} &>>/tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>>/tmp/${COMPONENT}.log
-statuscheck
+#echo starting ${COMPONENT} services
+#systemctl start ${COMPONENT} &>>/tmp/${COMPONENT}.log && systemctl enable ${COMPONENT} &>>/tmp/${COMPONENT}.log
+#statuscheck
