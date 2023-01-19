cb671e9d747fd9dcb31df87c7ab57da0138451ef
manudevops90 <manu.devops@gmail.com>
2023-01-19 19:42:17 +0530

    push code

--- Catalogue.sh
+++ Catalogue.sh
@@ -6,10 +6,10 @@ NODEJS
 
 
 
-echo configure catalogue systemd services
- mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>/tmp/catalogue.log &&  systemctl daemon-reload &>>/tmp/catalogue.log
-statuscheck
+#echo configure catalogue systemd services
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>/tmp/catalogue.log &&  systemctl daemon-reload &>>/tmp/catalogue.log
+#statuscheck
 
-echo starting catalogue services
- systemctl start catalogue &>>/tmp/catalogue.log && systemctl enable catalogue &>>/tmp/catalogue.log
-statuscheck
+#echo starting catalogue services
+ #systemctl start catalogue &>>/tmp/catalogue.log && systemctl enable catalogue &>>/tmp/catalogue.log
+#statuscheck
