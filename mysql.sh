21a5088e1d0335b6b3b418ee3d55c9fd9e0ee965
manudevops90 <manu.devops@gmail.com>
2023-01-26 07:46:37 +0530

    push code

--- mysql.sh
+++ mysql.sh
@@ -23,9 +23,9 @@ echo start mysql
 systemctl enable mysqld &>>{LOG} && systemctl start mysqld &>>{LOG}
 statuscheck
 
-DEFUAILT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
+DEFUAILT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}'}
 
-echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${log}
+#echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>${log}
 
 echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFUAILT_PASSWORD}
 
