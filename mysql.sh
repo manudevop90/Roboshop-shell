f0eb47d09d272cba596dfcb64da91cfb15cfa587
manudevops90 <manu.devops@gmail.com>
2023-01-26 07:14:15 +0530

    push code

--- mysql.sh
+++ mysql.sh
@@ -20,8 +20,8 @@ yum install mysql-community-server -y &>>{LOG}
 statuscheck
 
 echo start mysql
-systemctl enable mysqld &>>{LOG} && systemctl start mysqld &>>{LOG}
-statuscheck
+systemctl enable mysqld &>>{LOG} && systemctl start mysqld
+echo $?
 
 DEFUAILT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
