7bc05d12f96a7491c510f8d8f185ddb3c3c99921
manudevops90 <manu.devops@gmail.com>
2023-02-11 12:13:04 +0530

    push code

--- Mongodb.sh
+++ Mongodb.sh
@@ -1,6 +1,7 @@
 
-COMPONENT=mongodb
+
 source common.sh
+COMPONENT=mongodb
 
 echo setup yum repo
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
